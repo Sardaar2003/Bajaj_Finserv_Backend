c6621bdd5d13ddf1cf961ed082d027f90f96c55c
Sardaar2003 <singhmantej536@gmail.com>
2024-08-25 11:59:53 +0530

    Describe your changes here

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(cors());
 
 // Middleware to parse JSON request bodies
 app.use(bodyParser.json());
